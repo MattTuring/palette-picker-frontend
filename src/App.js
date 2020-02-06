3d0f5832c3129414d94c32ef2a15030dfd2a72f0
mattTuring <mjtowarnicky+1@gmail.com>
2020-02-05 17:48:52 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const randomColor = () => {
+  return '#'+Math.floor(Math.random()*16777215).toString(16)
+}
+
 function App() {
+
+  let [colors, setColors] = useState({
+    color1: randomColor(),
+    color2: randomColor(),
+    color3: randomColor(),
+    color4: randomColor(),
+    color5: randomColor(),
+    color1locked: false,
+    color2locked: false,
+    color3locked: false,
+    color4locked: false,
+    color5locked: false
+   })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+    <nav>COLORATOR</nav>
+    <section className="color-picker">
+      <div style={{backgroundColor: colors.color1}} className="colors color1">{colors.color1}</div>
+      <div style={{backgroundColor: colors.color2}} className="colors color2">{colors.color2}</div>
+      <div style={{backgroundColor: colors.color3}} className="colors color3">{colors.color3}</div>
+      <div style={{backgroundColor: colors.color4}} className="colors color4">{colors.color4}</div>
+      <div style={{backgroundColor: colors.color5}} className="colors color5">{colors.color5}</div>
+    </section>
+    <section className="projects">
+      <h2>Projects</h2>
+    </section>
+    </main>
   );
 }
 
