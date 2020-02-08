a194bf3cc699f0d46a83650f4c8c42fb304cade5
Raisa Primerova <riyuhide@gmail.com>
2020-02-07 23:58:54 -0700

    Create project item component

---
+++ src/Components/ProjectItem/ProjectItem.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { deleteProject, getPalettes } from '../../apiCalls/apiCalls';
+import trash from '../../trash.png';
+import edit from '../../edit.png';
+import ProjectPaletteItem from '../ProjectPaletteItem/ProjectPaletteItem'
+
+const ProjectItem = ({project, projects, setProjects}) => {
+  let [palettes, setPalettes] = useState([]);
+  const removeProject = async () => {
+    try {
+      const newProjects = projects.filter(prj => prj.id !== project.id);
+      setProjects(newProjects);
+      await deleteProject(project.id);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getPalettesForProject = async () => {
+    try {
+      const receivedPalettes = await getPalettes(project.id);
+      setPalettes(receivedPalettes);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  const paletteItems = palettes.map((palette, ind) => (
+    <ProjectPaletteItem
+    key={Date.now() + palette.name + ind}
+    {...{palettes, setPalettes, palette}} />
+  ));
+
+  return (
+    <>
+      <h4 key={project.id}>{project.name}
+        <img
+          src={trash}
+          onClick={removeProject}
+          className="delete"
+          alt='delete'/>
+        <img src={edit} className="edit" alt='edit'/>
+      </h4>
+        <button onClick={getPalettesForProject}>
+          Show/Update Palettes
+        </button>
+      {paletteItems}
+    </>
+  )
+};
+
+export default ProjectItem;
