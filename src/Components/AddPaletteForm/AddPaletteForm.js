792a4e717e7fa179430cc94f73dc925f59c106ef
Raisa Primerova <riyuhide@gmail.com>
2020-02-07 23:59:42 -0700

    Create add new palette form component

---
+++ src/Components/AddPaletteForm/AddPaletteForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const AddPaletteForm = (props) => {
+  const changeInput = (event) => {
+    props.setPaletteName(event.target.value);
+  };
+
+  const chooseProject = (event) => {
+    props.setCurrentProject(+event.target.value);
+  };
+
+  const submitPalette = async () => {
+    if(props.paletteName.length) {
+      props.addPalette();
+    }
+  };
+
+  const options = props.projects.map(project => (
+    <option value={project.id} key={project.id + project.id + Math.random}>{project.name}</option>
+  ));
+
+  return (
+    <section className="palettes">
+      <div>
+        <button onClick={props.assignToRandomColor}>New Colors</button>
+        <input
+          className="palette-name"
+          onChange={changeInput}
+          placeholder="Name"
+          type="text"/>
+        <select onChange={chooseProject}>
+          {(props.projects.length) && options }
+        </select>
+        <button onClick={submitPalette}>Add Palette</button>
+      </div>
+    </section>
+  )
+};
+
+export default AddPaletteForm;
