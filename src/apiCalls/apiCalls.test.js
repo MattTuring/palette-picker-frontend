39cc00392e68b246d3ae0a3a0770d71cc93093e0
Raisa Primerova <riyuhide@gmail.com>
2020-02-08 21:28:43 -0700

    Test GET request for palettes

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -51,3 +51,56 @@ describe("getProjects", () => {
   })
 });
 
+describe("getPalettes", () => {
+  const mockPalettes = [{
+    id: 1,
+    project_id: 'Project One',
+    color1: '#000000',
+    color2: '#ffffff',
+    color3: '#000000',
+    color4: '#ffffff',
+    color5: '#000000',
+  }];
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => {
+          return Promise.resolve(mockPalettes);
+        }
+      });
+    });
+  });
+
+  it("should call fetch with correct url", () => {
+    const url = 'https://palette-pick-backend.herokuapp.com/api/v1/projects/1/palettes';
+    getPalettes(1);
+    expect(window.fetch).toHaveBeenCalledWith(url);
+  })
+
+  it('should return an array with palettes', () => {
+    expect(getPalettes(1)).resolves.toEqual(mockPalettes);
+  })
+
+  it('should return an error for response that is not ok', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      });
+    });
+
+    expect(getPalettes()).rejects
+      .toEqual(Error("There was an error getting palettes."));
+  })
+
+  it('should return an error if fetch is rejected', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error('Failed to fetch'));
+    })
+
+    expect(getPalettes()).rejects
+      .toEqual(Error('Failed to fetch'));
+  })
+});
+
